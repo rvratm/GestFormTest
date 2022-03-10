bbdec1703189da129091473ba5638331cb5b144a
D0ntW <D0ntW@LAPTOP-2RV14CTJ>
2022-03-10 19:10:59 +0100

    Ajoutez des fichiers projet.

---
+++ TestTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
